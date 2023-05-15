be5fc184d4c56e3044ef93df18a592d408888912
phund01 <v.phund9@vinbigdata.org>
2023-05-16 00:52:18 +0700

    add time reg to gromov loss

--- scripts/run_multi_weizmman_knn.sh
+++ scripts/run_multi_weizmman_knn.sh
@@ -1,13 +1,16 @@
-#! /bin/bash
-for algo in parial-order-gromov gromov-dtw gromov partial-gromov; do
-    for i in {0..4}; do
-        python -m src.experiments.multi_features_weizmann.knn_eval \
-            --test_size 0.5 \
-            --outlier_ratio 0.1 \
-            --metric euclidean \
-            --m 0.7 \
-            --reg 10 \
-            --algo $algo
+# #! /bin/bash
+for algo in parial-order-gromov; do
+    for m in 0.6 0.7 0.8 0.9; do
+        for reg in 0.1 1 10 50 100; do
+            for i in {0..4}; do
+                python -m src.experiments.multi_features_weizmann.knn_eval \
+                    --test_size 0.5 \
+                    --random_outlier \
+                    --metric euclidean \
+                    --m $m \
+                    --reg $reg \
+                    --algo $algo
+            done
+        done
     done
 done
-
